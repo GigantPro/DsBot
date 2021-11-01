4e79dac635b83fee2b1b2f71b9bc6198c2a7533e
GigantPro <92632623+GigantPro@users.noreply.github.com>
2021-11-01 10:58:34 +0300

    1

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'ODg5MDkyNzA2OTM1MTI4MDc0.YUcOGw.eC5ticcDuJNogsM1KQb4vS6srbw',
+    'token': 'ODg5MDkyNzA2OTM1MTI4MDc0.YUcOGw.d7Z11d6fR3-otSy0EV_PVxfToDM',
     'bot': 'Crowbar Bot',
     'id': 889092706935128074,
     'prefix': '.',
