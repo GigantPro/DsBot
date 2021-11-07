b8b1b5d562367097f5a56b864f8a0cb9b27c96ad
GigantPro2000 <92632623+GigantPro@users.noreply.github.com>
2021-11-07 19:45:40 +0300

    StartUp

--- test.py
+++ test.py
@@ -1,40 +1,10 @@
-# import pymysql
-# host = "127.0.0.1"
-# user = "root"
-# password = "Rtrtph74"
-# db_name = "testBD"
+import getpass, os
+USER_NAME = getpass.getuser()
 
 
-# create_tabel = "CREATE TABLE IF NOT EXISTS '{tabel}'({args});"
-
-# print('Connecting to data base...', end='')
-# try:
-#     connection = pymysql.connect(
-#         host=host,
-#         port=3306,
-#         user=user,
-#         password=password,
-#         database=db_name,
-#         cursorclass=pymysql.cursors.DictCursor
-#     )
-#     print('Ok')
-# except Exception as ex:
-#     print('ERROR')
-#     print(ex)
-#     exit()
-# else:
-#     cur = connection.cursor()
-#     try:
-#         cur.execute(create_tabel.format(tabel='servers', args='id int AUTO_INCREMENT, ServerName varchar(32), ServerID int, AdminRoleName varchar(32), AdminRoleId int, CounterMembers int, CounterMaxWARNS int, WelcomeChatName varchar(32), WelcomeChatId int, WelcomeServerMessage varchar(32), WelcomeMessagePivate varchar(32), PRIMARY KEY (ServerID)'))
-#     except Exception as ex:
-#         print(create_tabel.format(tabel='servers', args='id int AUTO_INCREMENT, ServerName varchar(32), ServerID int, AdminRoleName varchar(32), AdminRoleId int, CounterMembers int, CounterMaxWARNS int, WelcomeChatName varchar(32), WelcomeChatId int, WelcomeServerMessage varchar(32), WelcomeMessagePivate varchar(32), PRIMARY KEY (ServerID)'))
-#         print('ERROR', ex)
-d = int(input())
-m = int(input())
-e = int(input()) 
-c = int(e / 100)
-y = ((e - c) % 100) 
-
-r = d + ((13 * m - 1) // 5) + y + (y // 4 + c // 4 - 2 * c + 777)
-r %= 7
-print(r)
+def add_to_startup(file_path=""):
+    if file_path == "":
+        file_path = os.path.dirname(os.path.realpath(__file__))
+    bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME
+    with open(bat_path + '\\' + "open.bat", "w+") as bat_file:
+        bat_file.write(r'start "" %s' % (file_path + '\discord_bot_main.py'))
